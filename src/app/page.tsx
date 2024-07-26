f87e9506641e4a01f83298de424234e5250f4c33
GarnierLenny <61317098+GarnierLenny@users.noreply.github.com>
2024-07-26 17:43:47 +0400

    FEAT - added scroll listenner to highlight current section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/jsx-key */
 "use client"
 import Image from "next/image";
-import { Inter } from 'next/font/google'
 import Header from "@/components/Header.component";
 import Hero from "@/components/sections/Hero.section";
 import Competences from "@/components/sections/Competences.section";
@@ -9,7 +8,7 @@ import Experiences from "@/components/sections/Experiences.section";
 import Projects from "@/components/sections/Projects.section";
 import Contact from "@/components/sections/Contact.section";
 import FAQ from "@/components/sections/Projects.section copy";
-import { ReactNode, useRef } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 
 export default function Home() {
   const sections: ReactNode[] = [
@@ -20,14 +19,28 @@ export default function Home() {
     <Contact />,
     <FAQ />,
   ];
-  const itemEls = useRef(new Array())
+  const itemEls = useRef(new Array());
+  const [selected, setSelected] = useState(0);
+
+  if (typeof window !== "undefined") {
+    window!.addEventListener('scrollend', () => {
+      const scrollY = window.scrollY;
+      console.log('Scroll position:', scrollY);
+      for (let i = 0; i < sections.length; i++) {
+        if (scrollY < itemEls.current[i].offsetTop + (window.innerHeight / 2)) {
+          setSelected(i);
+          break;
+        }
+      }
+    });
+  }
 
   return (
     <div>
-      <Header refs={itemEls} />
-      <main className="flex w-full flex-col self-end">
+      <Header refs={itemEls} values={{selected, setSelected}} />
+      <main className="flex w-full flex-col bg-yellow-900">
         {sections.map((section, index) => (
-          <div key={index} ref={(element) => {
+          <div className="flex self-end w-full justify-end" key={index} ref={(element) => {
             itemEls.current.push(element)
             }}>
             {section}
--- src/components/Header.component.tsx
+++ src/components/Header.component.tsx
@@ -1,22 +1,25 @@
 import palette from "@/utils/palette.utils";
 import React from "react";
 
-export default function Header({refs}) {
+export default function Header({refs, values}) {
   const sections: string[] = [
     "Hero",
-    "Competences",
+    "Skills",
     "Experience",
     "Projects",
     "Contact",
     "FAQ",
   ];
 
-  const executeScroll = (index: number) => refs.current[index].scrollIntoView();
+  const executeScroll = (index: number) => {
+    values.setSelected(index);
+    refs.current[index].scrollIntoView()
+  };
 
   return (
-    <div className="fixed flex h-full flex-col gap-y-4 justify-center px-14 py-2 left-0 self-center text-black" style={{backgroundColor: palette.yellow}}>
+    <div className="fixed flex h-full flex-col gap-y-4 justify-center px-10 py-2 left-0 self-center text-black" style={{backgroundColor: palette.yellow}}>
       {sections.map((section, index) => (
-        <div key={index} onClick={() => executeScroll(index)} className="cursor-pointer">{section}</div>
+        <div key={index} onClick={() => executeScroll(index)} className={`w-full cursor-pointer px-5 text-lg ${values.selected === index ? 'font-bold' : ''}`}>{section}</div>
       ))}
     </div>
   );
