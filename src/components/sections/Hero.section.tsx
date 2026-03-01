950d263dc903a3b05ee42ac68f63fb6351f71c14
GarnierLenny <lenny.garnier00@gmail.com>
2026-03-01 11:59:17 +0100

    radar chip mobile

--- src/components/sections/Hero.section.tsx
+++ src/components/sections/Hero.section.tsx
@@ -53,7 +53,7 @@ export default function Hero({ itemEls }: any) {
       </div>
 
       {/* Radar Chip */}
-      <div className="absolute top-6 right-6 z-50 hidden md:block">
+      <div className="absolute top-20 md:top-6 md:right-6">
         <div
           className="
             relative inline-flex items-center gap-3 px-4 py-2 rounded-full
